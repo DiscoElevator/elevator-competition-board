cc778914125f55cf0d245f789af3fd3fca74dfda
Vasya Redneck <vasya.redneck@gmail.com>
2016-11-24 17:32:30 +0300

    added fire

--- src/rating-place/rating-place.js
+++ src/rating-place/rating-place.js
@@ -1,8 +1,7 @@
 import {select} from "d3";
 import flow from "lodash/flow";
-import repeat from "lodash/repeat";
 
-import ratingPlaceTmpl from "./rating-place.pug";
+import {renderRatingPlaceTitle, updateRatingPlaceTitle} from "./rating-place__title/rating-place__title.js";
 import {renderBrickWall, updateBrickWallAnimation} from "../brick-wall/brick-wall.js";
 import {renderElevator, updateElevator} from "./rating-place__elevator/rating-place__elevator.js";
 
@@ -10,44 +9,24 @@ export {renderRatingPlace, updateRatingPlace};
 
 function renderRatingPlace() {
 	const ratingPlace = select(document.createElement("div"))
-		.html(ratingPlaceTmpl())
-		.select(".rating-place");
+		.attr("class", "rating-place");
 
-	ratingPlace.append(renderBrickWall);
+	ratingPlace.append(renderRatingPlaceTitle);
+	ratingPlace.append(renderBrickWall)
+		.classed("rating-place__brick-wall", true);
 	ratingPlace.append(renderElevator);
 
 	return ratingPlace.node();
 }
 
 function updateRatingPlace(ratingRecord) {
-	select(this)
+	select(this).select(".rating-place__title")
 		.each(updateRatingPlaceTitle);
 	select(this).select(".brick-wall")
 		.each(flow(getBrickWallAnimationDuration, updateBrickWallAnimation));
 	select(this).select(".rating-place__elevator")
 		.each(updateElevator)
 		.call(updateElevatorLayout, ratingRecord);
-}
-
-function updateRatingPlaceTitle(ratingRecord) {
-	const PLACE_FONT_SIZE = [10, 7, 5].map(size => `${size}vh`);
-
-	if (ratingRecord.place) {
-		select(this).select(".rating-place__title")
-			.style("visibility", "visible")
-			.style("font-size", PLACE_FONT_SIZE[ratingRecord.place]);
-		select(this).select(".rating-place__number")
-			.text(formatRatingPlaceNumber(ratingRecord));
-		select(this).select(".rating-place__name")
-			.text(ratingRecord.name);
-	} else {
-		select(this).select(".rating-place__title")
-			.style("visibility", "hidden");
-	}
-
-	function formatRatingPlaceNumber(ratingRecord) {
-		return repeat("I", ratingRecord.place);
-	}
 }
 
 function updateElevatorLayout(elevator, ratingRecord) {
